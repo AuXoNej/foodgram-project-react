bef5238f04da3459da99f69f56b7a44544a04e81
Алексей Бельянинов <i@a-belyaninov.ru>
2023-06-18 02:26:44 +0300

    version 5.7.1

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,7 +1,6 @@
 from django.forms.models import model_to_dict
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
 from recipes.models import (Favourite, Ingredient, IngredientAmount, Recipe,
                             ShoppingCart, Subscription, Tag)
 from rest_framework import filters, status
@@ -32,7 +31,7 @@ class RecipeViewSet(ModelViewSet):
 
     """
     filter_backends = (SearchFilter, DjangoFilterBackend)
-
+    from django_filters.rest_framework import DjangoFilterBackend
     filterset_fields = ('tags', )
     """
     filter_backends = (SearchFilter)
