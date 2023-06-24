d46576b948ac5fcaa159ef582850afad37e2ce31
Алексей Бельянинов <i@a-belyaninov.ru>
2023-06-25 00:46:15 +0300

    version 7.0.7 fix validators models

--- backend/foodgram/recipes/validators.py
+++ backend/foodgram/recipes/validators.py
@@ -8,26 +8,26 @@ validate_color = RegexValidator(
 
 
 def validate_name(value):
-    value = str(value)
+    name = str(value)
     alphabet_small = ('abcdefghijklmnopqrstuvwxyz'
                       'абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
     alphabet_big = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                     'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ')
 
-    for letter_index in len(value):
+    for letter_index in len(name):
         if not (value[letter_index] in alphabet_small
                 or value[letter_index] in alphabet_big):
             raise ValidationError(
-                'Поле должно содержать только буквы.'
+                'Поле может содержать только буквы.'
             )
 
         if letter_index == 0:
-            if not value[letter_index] in alphabet_big:
+            if not name[letter_index] in alphabet_big:
                 raise ValidationError(
-                    'Поле должно начинаться с заглавной буквы.'
+                    'В начале должна быть заглавная буква'
                 )
         else:
-            if not value[letter_index] in alphabet_small:
+            if not name[letter_index] in alphabet_small:
                 raise ValidationError(
                     'Заглавной может быть только первая буква.'
                 )
