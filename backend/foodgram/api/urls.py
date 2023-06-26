6d6832125414221c401a5746a44a434030059ffa
Алексей Бельянинов <i@a-belyaninov.ru>
2023-06-26 23:27:59 +0300

    version 8.1.16 fix actions

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -19,7 +19,7 @@ router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('', include(router_v1.urls), name='api_v1'),
-    path('', include('djoser.urls')),
+    path('users/', include('djoser.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
