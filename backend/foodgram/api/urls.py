1ffc8e41cfc5b119aacb5aebb17b2882d3404de0
Алексей Бельянинов <i@a-belyaninov.ru>
2023-06-27 00:10:43 +0300

    version 8.1.21 fix actions

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -18,7 +18,7 @@ router_v1.register('tags', TagViewSet, basename='tags')
 router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
-    path(r'users/', include('djoser.urls')),
+    path('users/', include('djoser.urls')),
     path('', include(router_v1.urls), name='api_v1'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
