2f84394a01df2cb77c475d5af6e68a0f5104e644
Алексей Бельянинов <i@a-belyaninov.ru>
2023-06-27 00:22:26 +0300

    version 8.1.23 fix actions

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -7,19 +7,22 @@ from .views import (IngredientViewSet, RecipeViewSet, SubscriptionViewSet,
 app_name = 'api'
 
 router_v1 = DefaultRouter()
-
+"""
 router_v1.register(
     'users',
     SubscriptionViewSet,
     basename='subscriptions'
 )
+"""
 router_v1.register('recipes', RecipeViewSet, basename='recipes')
 router_v1.register('tags', TagViewSet, basename='tags')
 router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
-    path('', include('djoser.urls')),
     path('', include(router_v1.urls), name='api_v1'),
+    path('users/subscriptions', SubscriptionViewSet.subscriptions),
+    path('users/subscribe', SubscriptionViewSet.subscribe),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
