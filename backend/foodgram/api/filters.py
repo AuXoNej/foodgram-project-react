49d9d8e57617514b347dd7be390ab0fd5f1a845d
Алексей Бельянинов <i@a-belyaninov.ru>
2023-06-26 17:23:48 +0300

    version version 8.0.7 fix filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -5,7 +5,7 @@ from users.models import User
 
 
 class RecipeFilter(FilterSet):
-    author = filters.ModelChoiceFilter(
+    author = filters.ModelMultipleChoiceFilter(
         field_name='author__id',
         queryset=User.objects.all()
     )
