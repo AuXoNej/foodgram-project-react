e17e71fe497e3bc948728a619aaf420da212104f
Алексей Бельянинов <i@a-belyaninov.ru>
2023-06-28 00:09:56 +0300

    version 8.1.45 fix actions

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -2,23 +2,32 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (IngredientViewSet, RecipeViewSet, SubscriptionViewSet,
-                    TagViewSet, subscribe)
+                    TagViewSet)
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 
-router_v1.register( 
-    'users/subscriptions', 
-    SubscriptionViewSet, 
-    basename='subscriptions' 
-)
 router_v1.register('recipes', RecipeViewSet, basename='recipes')
 router_v1.register('tags', TagViewSet, basename='tags')
 router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
-    path('users/<int:author_id>/subscribe/', subscribe, name='subscribe'),
+    path(
+        'users/subscriptions',
+        SubscriptionViewSet.as_view({'get': 'list'}),
+        name='subscriptions'
+    ),
+    path(
+        'users/<int:pk>/subscribe/',
+        SubscriptionViewSet.as_view(
+            {
+                'post': 'retrieve',
+                'delete': 'retrieve'
+            }
+        ),
+        name='subscribe'
+    ),
     path('', include(router_v1.urls), name='api_v1'),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls')),
