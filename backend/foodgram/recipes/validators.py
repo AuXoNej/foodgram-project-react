473b54058ca5d273c11964b0da194073ca0ae5ec
Алексей Бельянинов <i@a-belyaninov.ru>
2023-06-28 00:58:12 +0300

    version 8.2.0 fix validate_name

--- backend/foodgram/recipes/validators.py
+++ backend/foodgram/recipes/validators.py
@@ -6,27 +6,7 @@ validate_color = RegexValidator(
     'Не валидный hex-код'
 )
 
-
-def validate_name(value):
-    alphabet_small = ('abcdefghijklmnopqrstuvwxyz'
-                      'абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
-    alphabet_big = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                    'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ')
-
-    for letter_index in range(len(value)):
-        if not (value[letter_index] in alphabet_small
-                or value[letter_index] in alphabet_big):
-            raise ValidationError(
-                'Поле может содержать только буквы.'
-            )
-
-        if letter_index == 0:
-            if not value[letter_index] in alphabet_big:
-                raise ValidationError(
-                    'В начале должна быть заглавная буква'
-                )
-        else:
-            if not value[letter_index] in alphabet_small:
-                raise ValidationError(
-                    'Заглавной может быть только первая буква.'
-                )
+validate_name = RegexValidator(
+    r'^#([A-Za-zА-Яа-я])$',
+    'Поле может содержать только буквы.'
+)
