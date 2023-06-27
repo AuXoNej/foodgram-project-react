7dd7287043c8dee41a66c0db58d3f6c214db81b2
Алексей Бельянинов <i@a-belyaninov.ru>
2023-06-27 23:28:34 +0300

    version 8.1.37 fix actions

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (IngredientViewSet, RecipeViewSet, SubscriptionViewSet,
-                    TagViewSet, subscribe)
+                    TagViewSet)
 
 app_name = 'api'
 
