8b619dddb652f02aa43063b6ebe135ff5b9f25cc
Алексей <lyoshabelyaninov@yandex.com>
2023-06-29 23:48:59 +0300

    version 10.2.8 fix admin recipes

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -22,7 +22,6 @@ class RequiredInlineFormSet(BaseInlineFormSet):
 class RecipeTags(admin.TabularInline):
     model = Recipe.tags.through
     extra = 1
-    formset = RequiredInlineFormSet
 
 
 class RecipeIngredient(admin.TabularInline):
